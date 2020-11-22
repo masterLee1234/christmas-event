52e01fc880098378ff7fe7ec7ce0cd8b2632614b
masterLee1234 <47629824+masterLee1234@users.noreply.github.com>
2020-11-22 19:45:15 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 function clickSettingsBtn(){
     var element = document.querySelector(".settings");
     element.classList.toggle("visible");
-    console.log("clicked")
 }
--- js/settings.js
+++ js/settings.js
@@ -1,1 +1,2 @@
 let data = fetch('./json/data.json').then(results => results.json()).then(console.log)
+console.log(data) 
