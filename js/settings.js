868795db97661603691489e1dcb24e41dd672b8b
masterLee1234 <47629824+masterLee1234@users.noreply.github.com>
2020-11-22 23:13:21 +0900

    Add files via upload

--- js/settings.js
+++ js/settings.js
@@ -1,2 +1,8 @@
-let data = fetch('./json/data.json').then(results => results.json()).then(console.log)
-console.log(data) 
+let data = fetch('./data.txt').then(function(data){return JSON.parse(data)})
+let html = ''
+
+for(i=0;i<33;i++){
+    html += `<div class="data">학번: `+data.member[i].student_number + ' 이름: '+data.member[i].name+'    <img src="icon_1">  '+data.member[i].number+'  <img src="icon_2"></div>'
+}
+
+document.querySelector('.settings').innerHTML = html
